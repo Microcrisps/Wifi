1fcf3fa52ffc3e7623456855a39ab95ddea8e16e
Vincent Weltje <vincentweltje@gmail.com>
2018-11-07 14:18:58 +1000

    wifi script

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,65 @@
-#include <Arduino.h>
-#include "../conf/config.h"
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
-int fStartTime = 0;
-int fDelay = 30; // 30 ms
+const char* ssid = "Thrive 5G";
+const char* password = "qu!etSup3rPurple97";
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
 
-  int freq = 5000;
-  int resolution = 13;
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
 
-  ledcSetup(ledChannel, freq, resolution);
-  ledcAttachPin(ledPin, ledChannel);
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname("Demo OTA ESP32");
 
-  pinMode(buttonPin, INPUT);
+  // No authentication by default
+  ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-
-  // int sensorVal = digitalRead(buttonPin);
-  // Serial.println(buttonPin);
-  // if (sensorVal == HIGH) {
-  //   ledcWrite(ledChannel, 0);
-  // } else {
-  //   ledcWrite(ledChannel, 255);
-  // }
-
-  if (!loopSpeed) {
-    loopSpeed = getLoopSpeed();
-  } else {
-    int reading = digitalRead(buttonPin);
-    checkBtnState(reading);
-    animateLed();
-    lastButtonState = reading;
-  }
-  loopCount++;
-}
-
-int getLoopSpeed() {
-  if (!startTime) {
-    startTime = millis();
-  } else if((millis() - startTime) > checkTime) { // 0.1 miliseconds
-    loopSpeed = loopCount / checkTime; // get loop count per milisecond
-    loopCount = 0;
-  }
-  return loopSpeed;
-}
-
-int ms(int timeInMs) {
-  return (timeInMs * loopSpeed);
-}
-
-void checkBtnState(int reading) {
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
-  }
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != buttonState) {
-      buttonState = reading;
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-      }
-    }
-  }
-}
-
-void animateLed() {
-  if (ledState) {
-    if (loopCount < ms(100)) {
-      ledcWrite(ledChannel, 255);
-    } else if(loopCount < ms(200)) {
-      ledcWrite(ledChannel, 0);
-    } else {
-      loopCount = 0;
-    }
-  } else {
-    unsigned long now = millis();
-    if (!fStartTime) {
-      fStartTime = now;
-    } else if ((now - fStartTime) > fDelay) {
-      int valueMax = 255;
-      // calculate duty, 8191 from 2 ^ 13 - 1
-      uint32_t duty = (8191 / valueMax) * min(brightness, valueMax);
-      // write duty to LEDC
-      ledcWrite(ledChannel, duty);
-      brightness = brightness + fadeAmount;
-
-      if (brightness <= 0 || brightness >= 255) {
-        fadeAmount = -fadeAmount;
-      }
-      fStartTime = now;
-    }
-    loopCount = 0;
-  }
+  ArduinoOTA.handle();
 }
